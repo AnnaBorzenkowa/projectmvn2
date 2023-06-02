20a18f80319904ee8a6815363ef562a0cecff778
Anna Borzenkowa <crownedjocker@gmail.com>
2023-06-02 15:41:20 +0300

    Parametrized

--- src/test/java/ru/netology/service/FreelancerServiceTest.java
+++ src/test/java/ru/netology/service/FreelancerServiceTest.java
@@ -2,32 +2,37 @@ package ru.netology.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FreelancerServiceTest {
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+            "3, 10000, 3000, 20000",
+            "2, 100000, 60000, 150000"
+    })
 
-    public void testCalculateVacancy() {
+    public void testCalculateVacancy(int expected, int income, int expenses, int threshold) {
         FreelancerService service = new FreelancerService();
 
-        int actual = service.calculate(10000, 3000, 20000);
-        int expected = 3;
+        int actual = service.calculate(income, expenses, threshold);
+        //    int expected = 3;
 
         Assertions.assertEquals(expected, actual);
 
     }
 
-    @Test
+    //    @Test
 
-    public void testCalculateVacancy1() {
-        FreelancerService service = new FreelancerService();
+    //  public void testCalculateVacancy1() {
+    //      FreelancerService service = new FreelancerService();
 
-        int actual = service.calculate(100000, 60000, 150000);
-        int expected = 2;
+    //      int actual = service.calculate(100000, 60000, 150000);
+    //      int expected = 2;
 
-        Assertions.assertEquals(expected, actual);
-
-    }
+    //       Assertions.assertEquals(expected, actual);
 
 }
+
